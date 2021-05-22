8dd76656e35bc1a79a416713f21cd843f77e719f
kristof de spiegeleer <kristof@greenitglobe.com>
2021-05-22 10:33:47 +0200

    Update install_web_env.sh

--- scripts/install_web_env.sh
+++ scripts/install_web_env.sh
@@ -4,7 +4,7 @@ set -ex
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # check brew is installed
   if ! [ -x "$(command -v brew)" ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/crystaluniverse/publishtools/development/scripts/install.sh)"
